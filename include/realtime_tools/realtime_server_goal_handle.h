838fa98ca9c1d011d5737fc130aa2dbdb4d00f0e
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2018-06-20 19:32:23 +0200

    remove duplicated action pointer types  from RealtimeServerGoalHandle

--- include/realtime_tools/realtime_server_goal_handle.h
+++ include/realtime_tools/realtime_server_goal_handle.h
@@ -47,8 +47,6 @@ private:
   ACTION_DEFINITION(Action);
 
   typedef actionlib::ServerGoalHandle<Action> GoalHandle;
-  typedef std::shared_ptr<Result> ResultPtr;
-  typedef std::shared_ptr<Feedback> FeedbackPtr;
 
   uint8_t state_;
 
