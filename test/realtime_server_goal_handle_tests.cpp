c596da9af2564a1e62582f0b890ed4bfdbf3b800
Shane Loretz <sloretz@osrfoundation.org>
2019-05-30 10:54:12 -0700

    Port RealtimeServerGoalHandle to ROS 2
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- test/realtime_server_goal_handle_tests.cpp
+++ test/realtime_server_goal_handle_tests.cpp
@@ -29,45 +29,55 @@
 
 #include <gmock/gmock.h>
 #include <realtime_tools/realtime_server_goal_handle.h>
-#include <actionlib/client/simple_action_client.h>
-#include <actionlib/TwoIntsAction.h>
+#include <rclcpp/executors.hpp>
+#include <rclcpp/utilities.hpp>
+#include <rclcpp_action/create_client.hpp>
+#include <rclcpp_action/create_server.hpp>
+#include <test_msgs/action/fibonacci.hpp>
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <mutex>
 #include <thread>
 
+using test_msgs::action::Fibonacci;
+using ServerGoalHandle = rclcpp_action::ServerGoalHandle<Fibonacci>;
+using ClientGoalHandle = rclcpp_action::ClientGoalHandle<Fibonacci>;
 using realtime_tools::RealtimeServerGoalHandle;
-using TwoIntsActionClient = actionlib::SimpleActionClient<actionlib::TwoIntsAction>;
-using TwoIntsActionServer = actionlib::ActionServer<actionlib::TwoIntsAction>;
 
 const size_t ATTEMPTS = 10;
 const std::chrono::milliseconds DELAY(250);
 
-struct ActionCallback
+struct ActionServerCallbacks
 {
-  using GoalHandle = TwoIntsActionServer::GoalHandle;
   bool have_handle_ = false;
-  GoalHandle handle_;
+  std::shared_ptr<ServerGoalHandle> handle_;
   std::mutex mtx_;
 
-  void goal_callback(GoalHandle handle)
+  rclcpp_action::GoalResponse
+  goal_callback(const rclcpp_action::GoalUUID &, std::shared_ptr<const Fibonacci::Goal>)
+  {
+    return rclcpp_action::GoalResponse::ACCEPT_AND_DEFER;
+  }
+
+  void accepted_callback(std::shared_ptr<ServerGoalHandle> handle)
   {
     std::unique_lock<std::mutex> lock(mtx_);
     handle_ = handle;
-    handle_.setAccepted();
     have_handle_ = true;
   }
 
-  void cancel_callback(GoalHandle handle)
+  rclcpp_action::CancelResponse
+  cancel_callback(std::shared_ptr<ServerGoalHandle>)
   {
+    return rclcpp_action::CancelResponse::ACCEPT;
   }
 
-  bool wait_for_handle()
+  bool wait_for_handle(rclcpp::Node::SharedPtr node)
   {
     for (size_t i = 0; i < ATTEMPTS; ++i)
     {
-      ros::spinOnce();
+      rclcpp::spin_some(node);
       std::this_thread::sleep_for(DELAY);
       std::unique_lock<std::mutex> lock(mtx_);
       if (have_handle_)
@@ -80,23 +90,29 @@ struct ActionCallback
   }
 };
 
-struct FeedbackCallback
+struct ActionClientCallbacks
 {
   bool have_feedback_ = false;
+  bool have_result_ = false;
   std::mutex mtx_;
 
-  using FeedbackConstPtr = TwoIntsActionServer::FeedbackConstPtr;
-  void feedback_callback(const FeedbackConstPtr &)
+  void feedback_callback(ClientGoalHandle::SharedPtr, const Fibonacci::Feedback::ConstSharedPtr &)
   {
     std::unique_lock<std::mutex> lock(mtx_);
     have_feedback_ = true;
   }
 
-  bool wait_for_feedback()
+  void result_callback(const ClientGoalHandle::WrappedResult &)
+  {
+    std::unique_lock<std::mutex> lock(mtx_);
+    have_result_ = true;
+  }
+
+  bool wait_for_feedback(rclcpp::Node::SharedPtr node)
   {
     for (size_t i = 0; i < ATTEMPTS; ++i)
     {
-      ros::spinOnce();
+      rclcpp::spin_some(node);
       std::this_thread::sleep_for(DELAY);
       std::unique_lock<std::mutex> lock(mtx_);
       if (have_feedback_)
@@ -109,182 +125,197 @@ struct FeedbackCallback
   }
 };
 
-std::shared_ptr<TwoIntsActionClient>
+std::shared_ptr<ClientGoalHandle>
 send_goal(
+  rclcpp::Node::SharedPtr node,
+  std::shared_ptr<rclcpp_action::Client<Fibonacci>> ac,
   const std::string & server_name,
-  FeedbackCallback * cb = nullptr)
+  ActionClientCallbacks & client_callbacks)
 {
-  auto ac = std::make_shared<TwoIntsActionClient>(server_name, true);
-
-  for (size_t i = 0; i < ATTEMPTS && !ac->isServerConnected(); ++i)
+  for (size_t i = 0; i < ATTEMPTS && !ac->action_server_is_ready(); ++i)
   {
-    ros::spinOnce();
+    rclcpp::spin_some(node);
     std::this_thread::sleep_for(DELAY);
   }
-  if (!ac->isServerConnected())
+  if (ac->action_server_is_ready())
   {
-    ac.reset();
-  } else {
-    actionlib::TwoIntsGoal goal;
-    goal.a = 2;
-    goal.b = 3;
-    ac->sendGoal(
-      goal,
-      TwoIntsActionClient::SimpleDoneCallback(),
-      TwoIntsActionClient::SimpleActiveCallback(),
-      std::bind(&FeedbackCallback::feedback_callback, cb, std::placeholders::_1));
+    Fibonacci::Goal goal;
+    goal.order = 10;
+    // Create "SendGoalOptions" that has callbacks set
+    rclcpp_action::Client<Fibonacci>::SendGoalOptions send_goal_options;
+    send_goal_options.feedback_callback =
+      std::bind(&ActionClientCallbacks::feedback_callback, &client_callbacks, std::placeholders::_1, std::placeholders::_2);
+    send_goal_options.result_callback =
+      std::bind(&ActionClientCallbacks::result_callback, &client_callbacks, std::placeholders::_1);
+    // wait to get a client handle
+    auto future_handle = ac->async_send_goal(goal, send_goal_options);
+    rclcpp::spin_until_future_complete(node, future_handle);
+    return future_handle.get();
   }
-  return ac;
+  return nullptr;
 }
 
-bool wait_for_result(std::shared_ptr<TwoIntsActionClient> ac)
+std::shared_ptr<ClientGoalHandle::WrappedResult>
+wait_for_result(rclcpp::Node::SharedPtr node, std::shared_ptr<ClientGoalHandle> client_handle)
 {
-  for (int i = 0; i < ATTEMPTS; ++i)
-  {
-    ros::spinOnce();
-    if (ac->getState().isDone())
-    {
-      return true;
-    }
-    std::this_thread::sleep_for(DELAY);
-  }
-  return false;
+  // Get a result future
+  auto result_future = client_handle->async_result();
+  rclcpp::spin_until_future_complete(node, result_future);
+  return std::make_shared<ClientGoalHandle::WrappedResult>(result_future.get());
 }
 
-std::shared_ptr<TwoIntsActionServer>
-make_server(const std::string & server_name, ActionCallback & callbacks)
+rclcpp_action::Server<Fibonacci>::SharedPtr
+make_server(rclcpp::Node::SharedPtr node, const std::string & server_name, ActionServerCallbacks & callbacks)
 {
-  ros::NodeHandle nh;
-  auto as = std::make_shared<TwoIntsActionServer>(
-      nh, server_name,
-      std::bind(&ActionCallback::goal_callback, &callbacks, std::placeholders::_1),
-      std::bind(&ActionCallback::cancel_callback, &callbacks, std::placeholders::_1), false);
-  as->start();
-  return as;
+  return rclcpp_action::create_server<Fibonacci>(
+    node,
+    server_name,
+    std::bind(&ActionServerCallbacks::goal_callback, &callbacks, std::placeholders::_1, std::placeholders::_2),
+    std::bind(&ActionServerCallbacks::cancel_callback, &callbacks, std::placeholders::_1),
+    std::bind(&ActionServerCallbacks::accepted_callback, &callbacks, std::placeholders::_1));
 }
 
 TEST(RealtimeServerGoalHandle, set_aborted)
 {
-  ActionCallback callbacks;
-  const std::string server_name("set_aborted");
-  auto as = make_server(server_name, callbacks);
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("set_aborted");
+  ActionServerCallbacks callbacks;
+  ActionClientCallbacks client_callbacks;
+  const std::string server_name("~/client/set_aborted");
+  auto as = make_server(node, server_name, callbacks);
 
-  auto client = send_goal(server_name);
-  ASSERT_NE(nullptr, client.get());
+  auto client = rclcpp_action::create_client<Fibonacci>(node, server_name);
+  auto client_handle = send_goal(node, client, server_name, client_callbacks);
+  ASSERT_NE(nullptr, client_handle.get());
 
-  ASSERT_TRUE(callbacks.wait_for_handle());
-  RealtimeServerGoalHandle<actionlib::TwoIntsAction> rt_handle(callbacks.handle_);
+  ASSERT_TRUE(callbacks.wait_for_handle(node));
+  RealtimeServerGoalHandle<Fibonacci> rt_handle(callbacks.handle_);
   ASSERT_TRUE(rt_handle.valid());
+  rt_handle.execute();
+  rt_handle.runNonRealtime();
 
   {
-    actionlib::TwoIntsResult::Ptr result(new actionlib::TwoIntsResult);
-    result->sum = 42;
+    auto result = std::make_shared<Fibonacci::Result>();
+    result->sequence = {1, 1, 2, 3, 5};
     rt_handle.setAborted(result);
-    rt_handle.runNonRealtime(ros::TimerEvent());
+    rt_handle.runNonRealtime();
   }
 
-  ASSERT_TRUE(wait_for_result(client));
-  auto state = client->getState();
-  EXPECT_EQ(state, actionlib::SimpleClientGoalState::ABORTED) << state.toString();
-  auto result = client->getResult();
-  ASSERT_NE(nullptr, result.get());
-  EXPECT_EQ(42, result->sum);
+  auto wrapped_result = wait_for_result(node, client_handle);
+  ASSERT_NE(nullptr, wrapped_result.get());
+  EXPECT_EQ(wrapped_result->code, rclcpp_action::ResultCode::ABORTED);
+  ASSERT_NE(nullptr, wrapped_result->result.get());
+  EXPECT_EQ(5, wrapped_result->result->sequence.size());
+  rclcpp::shutdown();
 }
 
 TEST(RealtimeServerGoalHandle, set_canceled)
 {
-  ActionCallback callbacks;
-  const std::string server_name("set_canceled");
-  auto as = make_server(server_name, callbacks);
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("set_canceled");
+  ActionServerCallbacks callbacks;
+  ActionClientCallbacks client_callbacks;
+  const std::string server_name("~/client/set_canceled");
+  auto as = make_server(node, server_name, callbacks);
 
-  auto client = send_goal(server_name);
-  ASSERT_NE(nullptr, client.get());
+  auto client = rclcpp_action::create_client<Fibonacci>(node, server_name);
+  auto client_handle = send_goal(node, client, server_name, client_callbacks);
+  ASSERT_NE(nullptr, client_handle.get());
 
-  ASSERT_TRUE(callbacks.wait_for_handle());
-  RealtimeServerGoalHandle<actionlib::TwoIntsAction> rt_handle(callbacks.handle_);
+  ASSERT_TRUE(callbacks.wait_for_handle(node));
+  RealtimeServerGoalHandle<Fibonacci> rt_handle(callbacks.handle_);
   ASSERT_TRUE(rt_handle.valid());
+  rt_handle.execute();
+  rt_handle.runNonRealtime();
 
   // Cancel and wait for server to learn about that
-  client->cancelGoal();
+  client->async_cancel_goal(client_handle);
   for (size_t i = 0; i < ATTEMPTS; ++i)
   {
-    actionlib_msgs::GoalStatus gs = callbacks.handle_.getGoalStatus();
-    if (gs.status == actionlib_msgs::GoalStatus::PREEMPTING) {
+    if (callbacks.handle_->is_canceling())
+    {
       break;
     }
-    ros::spinOnce();
+    rclcpp::spin_some(node);
     std::this_thread::sleep_for(DELAY);
   }
-  ASSERT_EQ(callbacks.handle_.getGoalStatus().status, actionlib_msgs::GoalStatus::PREEMPTING);
+
+  ASSERT_TRUE(callbacks.handle_->is_canceling());
 
   {
-    actionlib::TwoIntsResult::Ptr result(new actionlib::TwoIntsResult);
-    result->sum = 1234;
+    auto result = std::make_shared<Fibonacci::Result>();
+    result->sequence = {1, 1, 2, 3, 5};
     rt_handle.setCanceled(result);
-    rt_handle.runNonRealtime(ros::TimerEvent());
+    rt_handle.runNonRealtime();
   }
 
-  ASSERT_TRUE(wait_for_result(client));
-  auto state = client->getState();
-  EXPECT_EQ(state, actionlib::SimpleClientGoalState::PREEMPTED) << state.toString();
-  auto result = client->getResult();
-  ASSERT_NE(nullptr, result.get());
-  EXPECT_EQ(1234, result->sum);
+  auto wrapped_result = wait_for_result(node, client_handle);
+  ASSERT_NE(nullptr, wrapped_result.get());
+  EXPECT_EQ(wrapped_result->code, rclcpp_action::ResultCode::CANCELED);
+  ASSERT_NE(nullptr, wrapped_result->result.get());
+  EXPECT_EQ(5, wrapped_result->result->sequence.size());
+  rclcpp::shutdown();
 }
 
 TEST(RealtimeServerGoalHandle, set_succeeded)
 {
-  ActionCallback callbacks;
-  const std::string server_name("set_succeeded");
-  auto as = make_server(server_name, callbacks);
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("set_succeeded");
+  ActionServerCallbacks callbacks;
+  ActionClientCallbacks client_callbacks;
+  const std::string server_name("~/client/set_succeeded");
+  auto as = make_server(node, server_name, callbacks);
 
-  auto client = send_goal(server_name);
-  ASSERT_NE(nullptr, client.get());
+  auto client = rclcpp_action::create_client<Fibonacci>(node, server_name);
+  auto client_handle = send_goal(node, client, server_name, client_callbacks);
+  ASSERT_NE(nullptr, client_handle.get());
 
-  ASSERT_TRUE(callbacks.wait_for_handle());
-  RealtimeServerGoalHandle<actionlib::TwoIntsAction> rt_handle(callbacks.handle_);
+  ASSERT_TRUE(callbacks.wait_for_handle(node));
+  RealtimeServerGoalHandle<Fibonacci> rt_handle(callbacks.handle_);
   ASSERT_TRUE(rt_handle.valid());
+  rt_handle.execute();
+  rt_handle.runNonRealtime();
 
   {
-    actionlib::TwoIntsResult::Ptr result(new actionlib::TwoIntsResult);
-    result->sum = 321;
+    auto result = std::make_shared<Fibonacci::Result>();
+    result->sequence = {1, 1, 2, 3, 5};
     rt_handle.setSucceeded(result);
-    rt_handle.runNonRealtime(ros::TimerEvent());
+    rt_handle.runNonRealtime();
   }
 
-  ASSERT_TRUE(wait_for_result(client));
-  auto state = client->getState();
-  EXPECT_EQ(state, actionlib::SimpleClientGoalState::SUCCEEDED) << state.toString();
-  auto result = client->getResult();
-  ASSERT_NE(nullptr, result.get());
-  EXPECT_EQ(321, result->sum);
+  auto wrapped_result = wait_for_result(node, client_handle);
+  ASSERT_NE(nullptr, wrapped_result.get());
+  EXPECT_EQ(wrapped_result->code, rclcpp_action::ResultCode::SUCCEEDED);
+  ASSERT_NE(nullptr, wrapped_result->result.get());
+  EXPECT_EQ(5, wrapped_result->result->sequence.size());
+  rclcpp::shutdown();
 }
 
 TEST(RealtimeServerGoalHandle, send_feedback)
 {
-  ActionCallback callbacks;
-  FeedbackCallback client_callbacks;
-  const std::string server_name("send_feedback");
-  auto as = make_server(server_name, callbacks);
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("send_feedback");
+  ActionServerCallbacks callbacks;
+  ActionClientCallbacks client_callbacks;
+  const std::string server_name("~/client/send_feedback");
+  auto as = make_server(node, server_name, callbacks);
 
-  auto client = send_goal(server_name, &client_callbacks);
-  ASSERT_NE(nullptr, client.get());
+  auto client = rclcpp_action::create_client<Fibonacci>(node, server_name);
+  auto client_handle = send_goal(node, client, server_name, client_callbacks);
+  ASSERT_NE(nullptr, client_handle.get());
 
-  ASSERT_TRUE(callbacks.wait_for_handle());
-  RealtimeServerGoalHandle<actionlib::TwoIntsAction> rt_handle(callbacks.handle_);
+  ASSERT_TRUE(callbacks.wait_for_handle(node));
+  RealtimeServerGoalHandle<Fibonacci> rt_handle(callbacks.handle_);
   ASSERT_TRUE(rt_handle.valid());
+  rt_handle.execute();
+  rt_handle.runNonRealtime();
 
   {
-    actionlib::TwoIntsFeedback::Ptr fb(new actionlib::TwoIntsFeedback());
+    auto fb = std::make_shared<Fibonacci::Feedback>();
     rt_handle.setFeedback(fb);
-    rt_handle.runNonRealtime(ros::TimerEvent());
+    rt_handle.runNonRealtime();
   }
 
-  EXPECT_TRUE(client_callbacks.wait_for_feedback());
-}
-
-int main(int argc, char ** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "realtime_server_goal_handle_tests");
-  return RUN_ALL_TESTS();
+  EXPECT_TRUE(client_callbacks.wait_for_feedback(node));
+  rclcpp::shutdown();
 }
