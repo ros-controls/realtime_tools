fc6f01a8428f2e859527aec30500b114fe45dd09
Kevin Jaget <kjaget@gmail.com>
2019-08-15 11:57:20 -0400

    Fix building realtime_publisher using NON_POLLING   Previously this tried wait on a condition variable using an   undefined lock. This commit adds the lock as a local var to   the publisher loop and uses it to wait when NON_POLLING is   defined. Additionally, it updates code to call the unlock()   method in various places to pick up conditional calls to   the condition var's notify_one() to wake up the cv's wait().

--- include/realtime_tools/realtime_publisher.h
+++ include/realtime_tools/realtime_publisher.h
@@ -124,7 +124,7 @@ public:
       }
       else
       {
-        msg_mutex_.unlock();
+        unlock();
         return false;
       }
     }
@@ -143,10 +143,7 @@ public:
   void unlockAndPublish()
   {
     turn_ = NON_REALTIME;
-    msg_mutex_.unlock();
-#ifdef NON_POLLING
-    updated_cond_.notify_one();
-#endif
+    unlock();
   }
 
   /**  \brief Get the data lock form non-realtime
@@ -174,6 +171,9 @@ public:
   void unlock()
   {
     msg_mutex_.unlock();
+#ifdef NON_POLLING
+    updated_cond_.notify_one();
+#endif
   }
 
 private:
@@ -188,13 +188,15 @@ private:
     thread_ = std::thread(&RealtimePublisher::publishingLoop, this);
   }
 
-
   bool is_running() const { return is_running_; }
 
   void publishingLoop()
   {
     is_running_ = true;
     turn_ = REALTIME;
+#ifdef NON_POLLING
+    std::unique_lock<std::mutex> cond_lock_(msg_mutex_, std::defer_lock_t());
+#endif
 
     while (keep_running_)
     {
@@ -205,7 +207,7 @@ private:
       while (turn_ != NON_REALTIME && keep_running_)
       {
 #ifdef NON_POLLING
-        updated_cond_.wait(lock);
+        updated_cond_.wait(cond_lock_);
 #else
         unlock();
         std::this_thread::sleep_for(std::chrono::microseconds(500));
