7738f4dffea1d6bea464a41cede4313fa48c2fb2
Denis Štogl <denis@stogl.de>
2022-08-22 15:17:53 +0200

    Apply suggestions from code review

--- include/realtime_tools/realtime_clock.h
+++ include/realtime_tools/realtime_clock.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2013 hiDOF, Inc.
+// Copyright (c) 2013, hiDOF, Inc.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
--- include/realtime_tools/thread_priority.hpp
+++ include/realtime_tools/thread_priority.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 PickNik Inc.
+// Copyright (c) 2022, PickNik, Inc.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
