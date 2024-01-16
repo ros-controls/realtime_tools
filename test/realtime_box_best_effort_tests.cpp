dce1458bac2e6d7e1ad4d5430ee2e1fbdd925170
Lennart Nachtigall <lennart.nachtigall@sci-mo.de>
2024-01-16 11:03:53 +0100

    added copyright notice

--- test/realtime_box_best_effort_tests.cpp
+++ test/realtime_box_best_effort_tests.cpp
@@ -1,3 +1,33 @@
+// Copyright (c) 2024, Lennart Nachtigall
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Willow Garage, Inc. nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+// Author: Lennart Nachtigall
+
 #include <gmock/gmock.h>
 #include <realtime_tools/realtime_box_best_effort.h>
 
@@ -56,18 +86,17 @@ TEST(RealtimeBoxBestEffort, non_default_constructable)
 }
 TEST(RealtimeBoxBestEffort, standard_get)
 {
-  RealtimeBoxBestEffort<DefaultConstructable> box(DefaultConstructable{.a=1000});
+  RealtimeBoxBestEffort<DefaultConstructable> box(DefaultConstructable{.a = 1000});
 
   DefaultConstructable data;
   box.get(data);
-  EXPECT_EQ(data.a,1000);
+  EXPECT_EQ(data.a, 1000);
   data.a = 10000;
-
 
   box.set(data);
 
   auto value = box.get();
-  EXPECT_EQ(value.a, 10000);  
+  EXPECT_EQ(value.a, 10000);
 }
 
 TEST(RealtimeBoxBestEffort, initializer_list)
@@ -124,7 +153,7 @@ TEST(RealtimeBoxBestEffort, pointer_type)
 
   box.get([](const auto & i) { EXPECT_EQ(*i, 200); });
 
-  box.tryGet([](const auto &i){EXPECT_EQ(*i,200);});
+  box.tryGet([](const auto & i) { EXPECT_EQ(*i, 200); });
 }
 
 TEST(RealtimeBoxBestEffort, smart_ptr_type)
