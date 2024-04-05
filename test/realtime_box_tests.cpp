ccd13a09d18c1e5ab03f0574e413dff060603df2
Lennart Nachtigall <mail@firesurfer.de>
2024-04-05 09:03:31 +0200

    run precommit

--- test/realtime_box_tests.cpp
+++ test/realtime_box_tests.cpp
@@ -171,7 +171,7 @@ TEST(RealtimeBox, smart_ptr_type)
 
   box.get([](const auto & i) { EXPECT_EQ(*i, 200); });
 
-    box.trySet([](const auto & p) { *p = 10; });
+  box.trySet([](const auto & p) { *p = 10; });
 
   box.tryGet([](const auto & p) { EXPECT_EQ(*p, 10); });
 }
