71141430f70c1ab9057cfd50edfc04e8ed905e85
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2024-11-24 18:43:36 +0100

    Fix RealtimeBox broken API + `realtime_box_best_effort.h` proper deprecation (#202)

--- include/realtime_tools/realtime_box.h
+++ include/realtime_tools/realtime_box.h
@@ -75,6 +75,7 @@ public:
     // We do not need to lock our own mutex because we are currently in the process of being created
     value_ = o.value_;
   }
+
   // Copy assignment constructor
   constexpr RealtimeBoxBase & operator=(const RealtimeBoxBase & o)
   {
@@ -88,6 +89,7 @@ public:
     }
     return *this;
   }
+
   constexpr RealtimeBoxBase(RealtimeBoxBase && o)
   {
     // Lock the other box mutex
@@ -95,6 +97,7 @@ public:
     // We do not need to lock our own mutex because we are currently in the process of being created
     value_ = std::move(o.value_);
   }
+
   // Only enabled for types that can be constructed from an initializer list
   template <typename U = T>
   constexpr RealtimeBoxBase(
@@ -103,6 +106,7 @@ public:
   : value_(init)
   {
   }
+
   constexpr RealtimeBoxBase & operator=(RealtimeBoxBase && o)
   {
     // Check for self assignment (and a potential deadlock)
@@ -131,6 +135,7 @@ public:
     value_ = value;
     return true;
   }
+
   /**
    * @brief access the content readable with best effort
    * @return false if the mutex could not be locked
@@ -146,6 +151,38 @@ public:
     func(value_);
     return true;
   }
+
+  /**
+   * @brief set a new content with best effort
+   * @return false if mutex could not be locked
+   * @note disabled for pointer types
+   * @deprecated Use try_set(const T & value) instead!
+   */
+  template <typename U = T>
+  [[deprecated("Use try_set(const T & value) instead!")]]
+  typename std::enable_if_t<!is_ptr_or_smart_ptr<U>, bool> trySet(const T & value)
+  {
+    std::unique_lock<mutex_t> guard(lock_, std::defer_lock);
+    if (!guard.try_lock()) {
+      return false;
+    }
+    value_ = value;
+    return true;
+  }
+
+  /**
+   * @brief access the content readable with best effort
+   * @return false if the mutex could not be locked
+   * @note only safe way to access pointer type content (rw)
+   * @deprecated Use try_set(const std::function<void(T &)> & func) instead!
+   */
+  template <typename U = T>
+  [[deprecated("Use try_set(const std::function<void(T &)> & func) instead!")]]
+  bool trySet(const std::function<void(T &)> & func)
+  {
+    return try_set(func);
+  }
+
   /**
    * @brief get the content with best effort
    * @return std::nullopt if content could not be access, otherwise the content is returned
@@ -159,6 +196,7 @@ public:
     }
     return value_;
   }
+
   /**
    * @brief access the content (r) with best effort
    * @return false if the mutex could not be locked
@@ -176,8 +214,34 @@ public:
   }
 
   /**
-   * @brief set the content and wait until the mutex could be locked (RealtimeBox behavior)
-   * @return true
+   * @brief get the content with best effort
+   * @return std::nullopt if content could not be access, otherwise the content is returned
+   * @deprecated Use try_get() instead!
+   */
+  template <typename U = T>
+  [[deprecated("Use try_get() instead!")]] [[nodiscard]]
+  typename std::enable_if_t<!is_ptr_or_smart_ptr<U>, std::optional<U>> tryGet() const
+  {
+    return try_get();
+  }
+
+  /**
+   * @brief access the content (r) with best effort
+   * @return false if the mutex could not be locked
+   * @note only safe way to access pointer type content (r)
+   * @deprecated Use try_get(const std::function<void(const T &)> & func) instead!
+   */
+  template <typename U = T>
+  [[deprecated("Use try_get(const std::function<void(const T &)> & func) instead!")]]
+  bool tryGet(const std::function<void(const T &)> & func)
+  {
+    return try_get(func);
+  }
+
+  /**
+   * @brief Wait until the mutex can be locked and set the content (RealtimeBox behavior)
+   * @note disabled for pointer types
+   * @note same signature as in the existing RealtimeBox<T>
    */
   template <typename U = T>
   typename std::enable_if_t<!is_ptr_or_smart_ptr<U>, void> set(const T & value)
@@ -186,17 +250,39 @@ public:
     // cppcheck-suppress missingReturn
     value_ = value;
   }
+
   /**
-   * @brief access the content (rw) and wait until the mutex could locked
+   * @brief Wait until the mutex can be locked and set the content (RealtimeBox behavior)
+   * @note same signature as in the existing RealtimeBox<T>
+   * @note Not the safest way to access pointer type content (rw)
+   * @deprecated Use set(const std::function<void(T &)> & func) instead!
+   */
+  template <typename U = T>
+  [[deprecated("Use set(const std::function<void(T &)> & func) instead!")]]
+  typename std::enable_if_t<is_ptr_or_smart_ptr<U>, void> set(const T & value)
+  {
+    std::lock_guard<mutex_t> guard(lock_);
+    // cppcheck-suppress missingReturn
+    value_ = value;
+  }
+
+  /**
+   * @brief wait until the mutex could be locked and access the content (rw)
    */
   void set(const std::function<void(T &)> & func)
   {
     std::lock_guard<mutex_t> guard(lock_);
+    if (!func) {
+      if constexpr (is_ptr_or_smart_ptr<T>) {
+        value_ = nullptr;
+        return;
+      }
+    }
     func(value_);
   }
 
   /**
-   * @brief get the content and wait until the mutex could be locked (RealtimeBox behaviour)
+   * @brief Wait until the mutex could be locked and get the content (RealtimeBox behaviour)
    * @return copy of the value
    */
   template <typename U = T>
@@ -205,8 +291,9 @@ public:
     std::lock_guard<mutex_t> guard(lock_);
     return value_;
   }
+
   /**
-   * @brief get the content and wait until the mutex could be locked
+   * @brief Wait until the mutex could be locked and get the content (r)
    * @note same signature as in the existing RealtimeBox<T>
    */
   template <typename U = T>
@@ -216,8 +303,24 @@ public:
     // cppcheck-suppress missingReturn
     in = value_;
   }
+
   /**
-   * @brief access the content (r) and wait until the mutex could be locked
+   * @brief Wait until the mutex could be locked and get the content (r)
+   * @note same signature as in the existing RealtimeBox<T>
+   * @note Not the safest way to access pointer type content (r)
+   * @deprecated Use get(const std::function<void(const T &)> & func) instead!
+   */
+  template <typename U = T>
+  [[deprecated("Use get(const std::function<void(const T &)> & func) instead!")]]
+  typename std::enable_if_t<is_ptr_or_smart_ptr<U>, void> get(T & in) const
+  {
+    std::lock_guard<mutex_t> guard(lock_);
+    // cppcheck-suppress missingReturn
+    in = value_;
+  }
+
+  /**
+   * @brief Wait until the mutex could be locked and access the content (r)
    * @note only safe way to access pointer type content (r)
    * @note same signature as in the existing RealtimeBox<T>
    */
@@ -247,6 +350,7 @@ public:
     // Only makes sense with the getNonRT method otherwise we would return an std::optional
     return get();
   }
+
   /**
    * @brief provide a custom conversion operator
    * @note Can be used from non-RT and RT contexts
@@ -263,6 +367,12 @@ public:
   // It may only be called from the thread that locked the mutex!
   [[nodiscard]] const mutex_t & get_mutex() const { return lock_; }
   [[nodiscard]] mutex_t & get_mutex() { return lock_; }
+
+  [[nodiscard]] [[deprecated("Use get_mutex() instead!")]] mutex_t & getMutex() { return lock_; }
+  [[nodiscard]] [[deprecated("Use get_mutex() instead!")]] const mutex_t & getMutex() const
+  {
+    return lock_;
+  }
 
 private:
   T value_;
