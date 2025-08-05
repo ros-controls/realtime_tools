872f9852d20467489953e89587fcd180e7a68f37
Dhruv Patel <dhruvpatel2991998@gmail.com>
2025-08-05 16:55:45 +0200

    Update realtime_tools/include/realtime_tools/mutex.hpp
    Co-authored-by: Sai Kishor Kothakota <saisastra3@gmail.com>

--- realtime_tools/include/realtime_tools/mutex.hpp
+++ realtime_tools/include/realtime_tools/mutex.hpp
@@ -202,7 +202,6 @@ public:
       }
       std::cerr << "Mutex owner died, but the mutex is consistent now. This shouldn't happen!"
                 << std::endl;
-      return true;  // Mutex is now consistent, we can continue using it
 #else
       std::cerr
         << "Mutex owner died, but pthread_mutex_consistent is not supported on this platform."
