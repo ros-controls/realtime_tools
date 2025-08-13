2627d53919a24b11c21bc4e088c5499a21c06124
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-08-13 10:29:15 +0200

    Add changes for the detached scheduling policy

--- realtime_tools/include/realtime_tools/async_function_handler.hpp
+++ realtime_tools/include/realtime_tools/async_function_handler.hpp
@@ -18,6 +18,7 @@
 #define REALTIME_TOOLS__ASYNC_FUNCTION_HANDLER_HPP_
 
 #include <atomic>
+#include <cmath>
 #include <condition_variable>
 #include <functional>
 #include <limits>
@@ -29,6 +30,7 @@
 #include <utility>
 #include <vector>
 
+#include "rclcpp/clock.hpp"
 #include "rclcpp/duration.hpp"
 #include "rclcpp/logging.hpp"
 #include "rclcpp/time.hpp"
@@ -36,6 +38,56 @@
 
 namespace realtime_tools
 {
+
+struct AsyncFunctionHandlerParams
+{
+  bool validate()
+  {
+    if (thread_priority < 0 || thread_priority > 99) {
+      RCLCPP_ERROR(
+        logger, "Invalid thread priority: %d. It should be between 0 and 99.", thread_priority);
+      return false;
+    }
+    if (scheduling_policy == AsyncSchedulingPolicy::DETACHED) {
+      if (!clock) {
+        RCLCPP_ERROR(logger, "Clock must be set when using DETACHED scheduling policy.");
+        return false;
+      }
+      if (exec_rate == 0u) {
+        RCLCPP_ERROR(logger, "Execution rate must be set when using DETACHED scheduling policy.");
+        return false;
+      }
+    }
+    if (trigger_predicate == nullptr) {
+      RCLCPP_ERROR(logger, "The parsed trigger predicate is not valid!");
+      return false;
+    }
+    return true;
+  }
+
+  /// @brief The AsyncFunctionHandlerParams struct is used to configure the AsyncFunctionHandler.
+  /// If the type is SYNCHRONIZED, the async worker thread will be synchronized with the main
+  /// thread, as the main thread will be triggering the async callback method.
+  /// If the type is DETACHED, the async worker thread will be detached from the main thread and
+  /// will have its own execution cycle.
+  enum class AsyncSchedulingPolicy {
+    SYNCHRONIZED,  /// Synchronized scheduling policy
+    DETACHED,      /// Detached scheduling policy
+  };
+  int thread_priority = 50;  /// Thread priority for the async worker thread
+  std::vector<int> cpu_affinity_cores =
+    {};  /// CPU cores to which the async worker thread should be pinned
+  AsyncSchedulingPolicy scheduling_policy =
+    AsyncSchedulingPolicy::SYNCHRONIZED;     /// Scheduling policy for the async worker thread
+  unsigned int exec_rate = 0u;               /// Execution rate of the async worker thread in Hz
+  rclcpp::Clock::SharedPtr clock = nullptr;  /// Clock to be used for the async worker thread
+  rclcpp::Logger logger =
+    rclcpp::get_logger("AsyncFunctionHandler");  /// Logger to be used for the async worker thread
+  std::function<bool()> trigger_predicate = []() {
+    return true;
+  };  /// Predicate function to check if the async callback method should be triggered or not
+};
+
 /**
  * @brief Class to handle asynchronous function calls.
  * AsyncFunctionHandler is a class that allows the user to have a asynchronous call to the parsed
@@ -98,6 +150,14 @@ public:
     trigger_predicate_ = trigger_predicate;
   }
 
+  void init(
+    std::function<T(const rclcpp::Time &, const rclcpp::Duration &)> callback,
+    const AsyncFunctionHandlerParams & params)
+  {
+    init(callback, params.trigger_predicate, params.thread_priority);
+    params_ = params;
+  }
+
   /// Triggers the async callback method cycle
   /**
    * @param time Current time
@@ -129,6 +189,13 @@ public:
         rclcpp::get_logger("AsyncFunctionHandler"),
         "AsyncFunctionHandler: Exception caught in the async callback thread!");
       std::rethrow_exception(async_exception_ptr_);
+    }
+    if (params_.scheduling_policy == AsyncFunctionHandlerParams::AsyncSchedulingPolicy::DETACHED) {
+      RCLCPP_WARN_ONCE(
+        rclcpp::get_logger("AsyncFunctionHandler"),
+        "AsyncFunctionHandler is configured with DETACHED scheduling policy. "
+        "This means that the async callback will not be synchronized with the main thread. ");
+      return std::make_pair(true, async_callback_return_.load(std::memory_order_relaxed));
     }
     if (!is_running()) {
       throw std::runtime_error(
@@ -297,33 +364,115 @@ public:
             "[https://control.ros.org/master/doc/ros2_control/controller_manager/doc/userdoc.html] "
             "for details.");
         }
-
-        while (!stop_async_callback_.load(std::memory_order_relaxed)) {
-          {
-            std::unique_lock<std::mutex> lock(async_mtx_);
-            async_callback_condition_.wait(
-              lock, [this] { return trigger_in_progress_ || stop_async_callback_; });
-            if (!stop_async_callback_) {
-              const auto start_time = std::chrono::steady_clock::now();
-              try {
-                async_callback_return_ =
-                  async_function_(current_callback_time_, current_callback_period_);
-              } catch (...) {
-                async_exception_ptr_ = std::current_exception();
-              }
-              const auto end_time = std::chrono::steady_clock::now();
-              last_execution_time_ =
-                std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-            }
-            trigger_in_progress_ = false;
-          }
-          cycle_end_condition_.notify_all();
+        if (!params_.cpu_affinity_cores.empty()) {
+          const auto affinity_result =
+            realtime_tools::set_current_thread_affinity(params_.cpu_affinity_cores);
+          RCLCPP_WARN_EXPRESSION(
+            params_.logger, !affinity_result.first,
+            "Could not set CPU affinity for the async worker thread. Error: %s",
+            affinity_result.second.c_str());
+          RCLCPP_WARN_EXPRESSION(
+            params_.logger, affinity_result.first,
+            "Async worker thread is successfully pinned to the requested CPU cores!");
+        }
+        if (
+          params_.scheduling_policy ==
+          AsyncFunctionHandlerParams::AsyncSchedulingPolicy::SYNCHRONIZED) {
+          execute_synchronized_callback();
+        } else {
+          execute_detached_callback();
         }
       });
     }
   }
 
 private:
+  void execute_synchronized_callback()
+  {
+    while (!stop_async_callback_.load(std::memory_order_relaxed)) {
+      {
+        std::unique_lock<std::mutex> lock(async_mtx_);
+        async_callback_condition_.wait(
+          lock, [this] { return trigger_in_progress_ || stop_async_callback_; });
+        if (!stop_async_callback_) {
+          const auto start_time = std::chrono::steady_clock::now();
+          try {
+            async_callback_return_ =
+              async_function_(current_callback_time_, current_callback_period_);
+          } catch (...) {
+            async_exception_ptr_ = std::current_exception();
+          }
+          const auto end_time = std::chrono::steady_clock::now();
+          last_execution_time_ =
+            std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
+        }
+        trigger_in_progress_ = false;
+      }
+      cycle_end_condition_.notify_all();
+    }
+  }
+
+  void execute_detached_callback()
+  {
+    if (!params_.clock) {
+      throw std::runtime_error(
+        "AsyncFunctionHandler: Clock must be set when using DETACHED scheduling policy.");
+    }
+    if (params_.exec_rate == 0u) {
+      throw std::runtime_error(
+        "AsyncFunctionHandler: Execution rate must be set when using DETACHED scheduling policy.");
+    }
+
+    auto const period = std::chrono::nanoseconds(1'000'000'000 / params_.exec_rate);
+
+    // for calculating the measured period of the loop
+    rclcpp::Time previous_time = params_.clock->now();
+    std::this_thread::sleep_for(period);
+    std::chrono::steady_clock::time_point next_iteration_time{std::chrono::steady_clock::now()};
+    while (!stop_async_callback_.load(std::memory_order_relaxed)) {
+      {
+        std::unique_lock<std::mutex> lock(async_mtx_);
+        async_callback_condition_.wait(
+          lock, [this] { return !pause_thread || stop_async_callback_; });
+        if (!stop_async_callback_) {
+          // calculate measured period
+          auto const current_time = params_.clock->now();
+          auto const measured_period = current_time - previous_time;
+          previous_time = current_time;
+          current_callback_time_ = current_time;
+          current_callback_period_ = measured_period;
+
+          const auto start_time = std::chrono::steady_clock::now();
+          try {
+            async_callback_return_ = async_function_(current_time, measured_period);
+          } catch (...) {
+            async_exception_ptr_ = std::current_exception();
+          }
+          last_execution_time_ = std::chrono::duration_cast<std::chrono::nanoseconds>(
+            std::chrono::steady_clock::now() - start_time);
+
+          next_iteration_time += period;
+          const auto time_now = std::chrono::steady_clock::now();
+          if (next_iteration_time < time_now) {
+            const double time_diff =
+              std::chrono::duration<double, std::micro>(time_now - next_iteration_time).count();
+            const double cm_period = 1.e3 / static_cast<double>(params_.exec_rate);
+            const int overrun_count = static_cast<int>(std::ceil(time_diff / cm_period));
+            RCLCPP_WARN_THROTTLE(
+              params_.logger, *params_.clock, 1000,
+              "Overrun detected! The async callback missed its desired rate of %d Hz. The loop "
+              "took %f ms (missed cycles : %d).",
+              params_.exec_rate, time_diff + cm_period, overrun_count + 1);
+            next_iteration_time += (overrun_count * period);
+          }
+          std::this_thread::sleep_until(next_iteration_time);
+        }
+        trigger_in_progress_ = false;
+      }
+      cycle_end_condition_.notify_all();
+    }
+  }
+
   rclcpp::Time current_callback_time_ = rclcpp::Time(0, 0, RCL_CLOCK_UNINITIALIZED);
   rclcpp::Duration current_callback_period_{0, 0};
 
@@ -332,9 +481,11 @@ private:
 
   // Async related variables
   std::thread thread_;
+  AsyncFunctionHandlerParams params_;
   int thread_priority_ = std::numeric_limits<int>::quiet_NaN();
   std::atomic_bool stop_async_callback_{false};
   std::atomic_bool trigger_in_progress_{false};
+  std::atomic_bool pause_thread{false};
   std::atomic<T> async_callback_return_;
   std::condition_variable async_callback_condition_;
   std::condition_variable cycle_end_condition_;
