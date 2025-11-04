59ff73bf289d71a3c9252777d479fa1acbb09172
Abdullah <88815725+Theonewhomadethings@users.noreply.github.com>
2025-11-04 11:52:02 +0000

    Replace deprecated spin_some in realtime_tools (#448)

--- realtime_tools/test/realtime_publisher_tests.cpp
+++ realtime_tools/test/realtime_publisher_tests.cpp
@@ -88,8 +88,11 @@ TEST(RealtimePublisher, rt_can_try_publish)
   auto sub = node->create_subscription<StringMsg>(
     "~/rt_publish", qos,
     std::bind(&StringCallback::callback, &str_callback, std::placeholders::_1));
+
+  rclcpp::executors::SingleThreadedExecutor exec;
+  exec.add_node(node);
   for (size_t i = 0; i < ATTEMPTS && str_callback.msg_.string_value.empty(); ++i) {
-    rclcpp::spin_some(node);
+    exec.spin_some();
     std::this_thread::sleep_for(DELAY);
   }
   EXPECT_STREQ(expected_msg, str_callback.msg_.string_value.c_str());
