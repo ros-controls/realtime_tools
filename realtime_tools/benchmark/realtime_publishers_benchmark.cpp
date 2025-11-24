08ed5ebd9897ad75a1177da4ccc06c13ab3378f1
Brian Jin <brian.jin.207@gmail.com>
2025-11-23 23:47:46 -0500

    init benchmark impl

--- realtime_tools/benchmark/realtime_publishers_benchmark.cpp
+++ realtime_tools/benchmark/realtime_publishers_benchmark.cpp
@@ -16,24 +16,58 @@
 
 #include <benchmark/benchmark.h>
 
+#include <memory>
 #include <string>
 
+#include <test_msgs/msg/empty.hpp>
+
 #include <realtime_tools/realtime_publisher.hpp>
+#include <realtime_tools/utils/publisher_interface.hpp>
 #include <realtime_tools/wait_free_realtime_publisher.hpp>
 
-static void BM_StringCreation(benchmark::State & state)
+namespace
 {
-  for (auto _ : state) std::string empty_string;
+template <class MessageT>
+class BenchmarkPublisher : public realtime_tools::utils::PublisherInterface<MessageT>
+{
+public:
+  void publish(const MessageT &) override { publish_count_++; }
+
+  double count() const { return publish_count_; }
+
+private:
+  double publish_count_{0.0};
+};
+}  // namespace
+
+static void BM_RealtimePublisher(benchmark::State & state)
+{
+  auto pub = std::make_shared<BenchmarkPublisher<test_msgs::msg::Empty>>();
+  realtime_tools::RealtimePublisher<test_msgs::msg::Empty> rt_pub(pub);
+
+  test_msgs::msg::Empty msg;
+  for (auto _ : state) {
+    rt_pub.try_publish(msg);
+  }
+
+  state.counters["num_publishes"] = pub->count();
 }
 // Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK(BM_RealtimePublisher);
 
 // Define another benchmark
-static void BM_StringCopy(benchmark::State & state)
+static void BM_WaitFreeRealtimePublisher(benchmark::State & state)
 {
-  std::string x = "hello";
-  for (auto _ : state) std::string copy(x);
+  auto pub = std::make_shared<BenchmarkPublisher<test_msgs::msg::Empty>>();
+  realtime_tools::WaitFreeRealtimePublisher<test_msgs::msg::Empty> rt_pub(pub);
+
+  test_msgs::msg::Empty msg;
+  for (auto _ : state) {
+    rt_pub.push(msg);
+  }
+
+  state.counters["num_publishes"] = pub->count();
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_WaitFreeRealtimePublisher);
 
 BENCHMARK_MAIN();
