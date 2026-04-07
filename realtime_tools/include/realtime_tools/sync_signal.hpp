cceaa02919e6c0296ae392baf4543305ee952e69
nibanovic <nikola.banovic@b-robotized.com>
2026-04-07 14:23:50 +0200

    add a timeout to wait_for_read_signal_finished + minor changes:
    * add docs for functions
    * const correctness
    * add std::optional<> to catch timeouts when waiting for signal, to starndardize usage
    * pre-commit

--- realtime_tools/include/realtime_tools/sync_signal.hpp
+++ realtime_tools/include/realtime_tools/sync_signal.hpp
@@ -1,112 +1,151 @@
-#ifndef REALTIME_TOOLS__SYNC_SIGNAL
-#define REALTIME_TOOLS__SYNC_SIGNAL
+// Copyright 2026 b-robotized GmbH.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#include <mutex>
-#include <condition_variable>
+/// \author Nikola Banovic
+
+#ifndef REALTIME_TOOLS__SYNC_SIGNAL_HPP_
+#define REALTIME_TOOLS__SYNC_SIGNAL_HPP_
+
 #include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <mutex>
+#include <optional>
 
-namespace realtime_tools 
+namespace realtime_tools
 {
 
 /*
-Used to synchronize threads of hardware interface read calls with update() functions of async controllers.
-
-Slave async hardware interfaces run on robot clock by blocking in read() function.
-We want to be able to attach n async controllers to be woken up and their update() functions run after the read() finishes.
-
-This class enables this functionality.
+* Used to synchronize threads of hardware interface read calls with update() functions of async controllers.
+*
+* Slave async hardware interfaces run on robot clock by blocking in read() function.
+* We want to be able to attach n async controllers to be woken up and their update() functions run after the read() finishes.
+*
+* This class enables this functionality.
+*
+* Example flow:
+*   HW read() -> signal_read_finished() -> controllers wake
+*   -> controller update() -> signal_update_finished()
+*   -> (all controllers done) -> HW write() proceeds
+*
 */
-class SyncSignal{
+class SyncSignal
+{
 public:
-
-    // HW read -> Controller update signal path
-    void signal_read_finished() {
-        auto now = std::chrono::steady_clock::now().time_since_epoch().count();
-        last_signal_read_finished_time_.store(now, std::memory_order_relaxed);
-        {
-            std::lock_guard<std::mutex> lock(mutex_);
-            completed_updates_ = 0;
-            cycle_counter_++;
-        }
-        cv_hw_to_ctrl_.notify_all();
+  /// HW read -> Controller update signal path
+  void signal_read_finished()
+  {
+    auto now = std::chrono::steady_clock::now().time_since_epoch().count();
+    last_signal_read_finished_time_.store(now, std::memory_order_relaxed);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      completed_updates_ = 0;
+      cycle_counter_++;
     }
+    cv_hw_to_ctrl_.notify_all();
+  }
 
-    uint64_t wait_for_signal_read_finished(){
-        std::unique_lock<std::mutex> lock(mutex_);
+  /// called by the controller to wait for the next read_finished signal.
+  /// @return The cycle counter if the signal was received, std::nullopt if timed out.
+  std::optional<uint64_t> wait_for_signal_read_finished(std::chrono::nanoseconds timeout)
+  {
+    std::unique_lock<std::mutex> lock(mutex_);
+    uint64_t cycle_count_current = cycle_counter_;
+    bool signaled = cv_hw_to_ctrl_.wait_for(
+      lock, timeout, [this, cycle_count_current] { return cycle_counter_ > cycle_count_current; });
 
-        uint64_t cycle_count_current = cycle_counter_;
-        cv_hw_to_ctrl_.wait(lock,
-            [this, cycle_count_current]{
-                return cycle_counter_ > cycle_count_current;
-            }
-        );
-
-        return cycle_counter_;
+    if (!signaled) {
+      return std::nullopt;
     }
+    return cycle_counter_;
+  }
 
-    // Controller update ->  HW write signal path
-    void register_controller() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        num_updates_to_wait_on_++;
+  /// Controller update ->  HW write signal path
+
+  /// register that a controller as requires synchronization. Called during activation.
+  void register_controller()
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    num_updates_to_wait_on_++;
+  }
+
+  /// Unregister a controller. Called on controller deactivation/error/shutdown.
+  void unregister_controller()
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (num_updates_to_wait_on_ > 0) {
+      num_updates_to_wait_on_--;
     }
-
-    void unregister_controller() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        if (num_updates_to_wait_on_ > 0) num_updates_to_wait_on_--;
-        // if a controller which hw is waiting on is deactivated, wake the hardware up
-        if (completed_updates_ >= num_updates_to_wait_on_) {
-            cv_ctrl_to_hw_.notify_one();
-        }
+    // If hardware is waiting on updates and this was the last one, wake it up.
+    if (completed_updates_ >= num_updates_to_wait_on_) {
+      cv_ctrl_to_hw_.notify_one();
     }
+  }
 
-    void signal_update_finished() {
-        std::lock_guard<std::mutex> lock(mutex_);
-        completed_updates_++;
-        if (completed_updates_ >= num_updates_to_wait_on_) {
-            auto now = std::chrono::steady_clock::now().time_since_epoch().count();
-            last_signal_update_finished_time_.store(now, std::memory_order_relaxed);
-
-            cv_ctrl_to_hw_.notify_one();
-        }
+  /// called by controller after update() finishes.
+  void signal_update_finished()
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    completed_updates_++;
+    if (completed_updates_ >= num_updates_to_wait_on_) {
+      auto now = std::chrono::steady_clock::now().time_since_epoch().count();
+      last_signal_update_finished_time_.store(now, std::memory_order_relaxed);
+      cv_ctrl_to_hw_.notify_one();
     }
+  }
 
-    bool wait_for_signal_updates_finished(std::chrono::nanoseconds timeout) {
-        std::unique_lock<std::mutex> lock(mutex_);
-        return cv_ctrl_to_hw_.wait_for(lock, timeout, [this] { 
-            return completed_updates_ >= num_updates_to_wait_on_; 
-        });
-    }
+  /// called by hardware to wait for all registered controllers to finish update().
+  /// @return true if all updates completed, std::nullopt if timed out.
+  std::optional<bool> wait_for_signal_updates_finished(std::chrono::nanoseconds timeout)
+  {
+    std::unique_lock<std::mutex> lock(mutex_);
+    bool completed = cv_ctrl_to_hw_.wait_for(
+      lock, timeout, [this] { return completed_updates_ >= num_updates_to_wait_on_; });
 
-    int get_num_updates_hw_waits_on() {
-        return num_updates_to_wait_on_;
+    if (!completed) {
+      return std::nullopt;
     }
+    return true;
+  }
 
-    uint64_t get_last_signal_read_finished_time() const {
-        return last_signal_read_finished_time_.load(std::memory_order_relaxed);
-    }
+  int get_num_updates_hw_waits_on() const { return num_updates_to_wait_on_; }
 
-    uint64_t get_last_signal_update_finished_time() const {
-        return last_signal_update_finished_time_.load(std::memory_order_relaxed);
-    }
+  uint64_t get_last_signal_read_finished_time() const
+  {
+    return static_cast<uint64_t>(last_signal_read_finished_time_.load(std::memory_order_relaxed));
+  }
 
-    uint64_t get_cycle_counter() const {
-        return cycle_counter_;
-    }
+  uint64_t get_last_signal_update_finished_time() const
+  {
+    return static_cast<uint64_t>(last_signal_update_finished_time_.load(std::memory_order_relaxed));
+  }
+
+  uint64_t get_cycle_counter() const { return cycle_counter_; }
 
 private:
-    std::mutex mutex_;
-    std::condition_variable cv_hw_to_ctrl_;
-    std::condition_variable cv_ctrl_to_hw_;
+  std::mutex mutex_;
+  std::condition_variable cv_hw_to_ctrl_;
+  std::condition_variable cv_ctrl_to_hw_;
 
-    int num_updates_to_wait_on_ = 0;
-    int completed_updates_ = 0;
+  int num_updates_to_wait_on_ = 0;
+  int completed_updates_ = 0;
 
-    uint64_t cycle_counter_ = 0; // prevents spurious wakeups.
-    std::atomic<int64_t> last_signal_read_finished_time_{0};
-    std::atomic<int64_t> last_signal_update_finished_time_{0};
+  uint64_t cycle_counter_ = 0;  // prevents spurious wakeups.
+  std::atomic<int64_t> last_signal_read_finished_time_{0};
+  std::atomic<int64_t> last_signal_update_finished_time_{0};
 };
 
+}  // namespace realtime_tools
 
-} // namespace hardware_interface
-
-#endif  // REALTIME_TOOLS__SYNC_SIGNAL
+#endif  // REALTIME_TOOLS__SYNC_SIGNAL_HPP_
