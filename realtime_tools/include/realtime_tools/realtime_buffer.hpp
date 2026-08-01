3d52cb3deeef509691d86d2ef0bbdce0d0b78c45
SouriRishik <vsouri2705@gmail.com>
2026-08-01 15:16:01 +0530

    refactor: update deprecation messages to point to guidelines

--- realtime_tools/include/realtime_tools/realtime_buffer.hpp
+++ realtime_tools/include/realtime_tools/realtime_buffer.hpp
@@ -39,17 +39,21 @@
 
 #ifndef SILENCE_DEPRECATION_WARNINGS
 #ifdef _MSC_VER
-#pragma message( \
-  "This header is obsolete, please use \"realtime_tools/realtime_thread_safe_box.hpp\" instead.")
+#pragma message(                                                                      \
+  "This header is obsolete, please refer to the guidelines for replacement options: " \
+  "control.ros.org/rolling/doc/realtime_tools/doc/index.html#guidelines")
 #else
-#warning This header is obsolete, please use "realtime_tools/realtime_thread_safe_box.hpp" instead.
+#warning This header is obsolete, please refer to the guidelines for replacement options: \
+control.ros.org/rolling/doc/realtime_tools/doc/index.html#guidelines
 #endif
 #endif
 
 namespace realtime_tools
 {
 template <class T>
-class [[deprecated("Use realtime_tools::RealtimeThreadSafeBox instead.")]] RealtimeBuffer
+class [[deprecated(
+  "refer to the guidelines for replacement options: "
+  "control.ros.org/rolling/doc/realtime_tools/doc/index.html#guidelines")]] RealtimeBuffer
 {
 public:
   RealtimeBuffer() : new_data_available_(false)
