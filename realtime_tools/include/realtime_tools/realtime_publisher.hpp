af4ab1b2c45d0a0c6ea10d130e09a1bf35c97f7a
SouriRishik <vsouri2705@gmail.com>
2026-08-05 12:44:30 +0530

    feat: create publisher internally in RealtimePublisher

--- realtime_tools/include/realtime_tools/realtime_publisher.hpp
+++ realtime_tools/include/realtime_tools/realtime_publisher.hpp
@@ -46,6 +46,7 @@
 #include <thread>
 #include <utility>
 
+#include "rclcpp/create_publisher.hpp"
 #include "rclcpp/publisher.hpp"
 
 namespace realtime_tools
@@ -64,6 +65,39 @@ public:
   RCLCPP_SMART_PTR_DEFINITIONS(RealtimePublisher<MessageT>)
 
   /**
+   * \brief Constructor for the realtime publisher that creates the publisher internally
+   *
+   * Starts a dedicated thread for message publishing.
+   * The publishing thread runs the publishingLoop() function to handle message
+   * delivery in a non-realtime context.
+   *
+   * \param node the node (or node interface/pointer) to create the publisher for
+   * \param topic_name the topic name on which we want to publish
+   * \param qos the QoS settings for the publisher
+   * \param options the publisher options
+   */
+  template <typename NodeT>
+  explicit RealtimePublisher(
+    NodeT && node, const std::string & topic_name, const rclcpp::QoS & qos,
+    const rclcpp::PublisherOptions & options = rclcpp::PublisherOptions())
+  : publisher_(
+      rclcpp::create_publisher<MessageT>(std::forward<NodeT>(node), topic_name, qos, options)),
+    is_running_(false),
+    keep_running_(true),
+    turn_(State::LOOP_NOT_STARTED)
+  {
+    thread_ = std::thread(&RealtimePublisher::publishingLoop, this);
+
+    // Wait for the thread to be ready before proceeding
+    // This is important to ensure that the thread is properly initialized and ready to handle
+    // messages before any other operations are performed on the RealtimePublisher instance.
+    while (!thread_.joinable() ||
+           turn_.load(std::memory_order_acquire) == State::LOOP_NOT_STARTED) {
+      std::this_thread::sleep_for(std::chrono::microseconds(100));
+    }
+  }
+
+  /**
    * \brief Constructor for the realtime publisher
    *
    * Starts a dedicated thread for message publishing.
@@ -72,6 +106,7 @@ public:
    *
    * \param publisher the ROS publisher to wrap
    */
+  [[deprecated("Use the constructor that creates the publisher internally instead.")]]
   explicit RealtimePublisher(PublisherSharedPtr publisher)
   : publisher_(publisher), is_running_(false), keep_running_(true), turn_(State::LOOP_NOT_STARTED)
   {
--- realtime_tools/test/realtime_publisher_tests.cpp
+++ realtime_tools/test/realtime_publisher_tests.cpp
@@ -64,8 +64,7 @@ TEST(RealtimePublisher, rt_can_try_publish)
   auto node = std::make_shared<rclcpp::Node>("construct_move_destruct");
   rclcpp::QoS qos(10);
   qos.reliable().transient_local();
-  auto pub = node->create_publisher<StringMsg>("~/rt_publish", qos);
-  RealtimePublisher<StringMsg> rt_pub(pub);
+  RealtimePublisher<StringMsg> rt_pub(node, "~/rt_publish", qos);
   ASSERT_TRUE(rt_pub.can_publish());
 
   // try publish a latched message
@@ -87,6 +86,67 @@ TEST(RealtimePublisher, rt_can_try_publish)
 
   auto sub = node->create_subscription<StringMsg>(
     "~/rt_publish", qos,
+    std::bind(&StringCallback::callback, &str_callback, std::placeholders::_1));
+
+  rclcpp::executors::SingleThreadedExecutor exec;
+  exec.add_node(node);
+  for (size_t i = 0; i < ATTEMPTS && str_callback.msg_.string_value.empty(); ++i) {
+    exec.spin_some();
+    std::this_thread::sleep_for(DELAY);
+  }
+  EXPECT_STREQ(expected_msg, str_callback.msg_.string_value.c_str());
+  rclcpp::shutdown();
+}
+
+TEST(RealtimePublisher, rt_can_try_publish_deprecated)
+{
+  rclcpp::init(0, nullptr);
+  const size_t ATTEMPTS = 10;
+  const std::chrono::milliseconds DELAY(250);
+
+  const char * expected_msg = "Hello World";
+  auto node = std::make_shared<rclcpp::Node>("construct_move_destruct_deprecated");
+  rclcpp::QoS qos(10);
+  qos.reliable().transient_local();
+  auto pub = node->create_publisher<StringMsg>("~/rt_publish_dep", qos);
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#endif
+#if defined(__clang__) || defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+  RealtimePublisher<StringMsg> rt_pub(pub);
+#if defined(__clang__) || defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
+  ASSERT_TRUE(rt_pub.can_publish());
+
+  // try publish a latched message
+  bool publish_success = false;
+  for (std::size_t i = 0; i < ATTEMPTS; ++i) {
+    StringMsg msg;
+    msg.string_value = expected_msg;
+
+    if (rt_pub.can_publish()) {
+      ASSERT_TRUE(rt_pub.try_publish(msg));
+      publish_success = true;
+    }
+    std::this_thread::sleep_for(DELAY);
+  }
+  ASSERT_TRUE(publish_success);
+
+  // make sure subscriber gets it
+  StringCallback str_callback;
+
+  auto sub = node->create_subscription<StringMsg>(
+    "~/rt_publish_dep", qos,
     std::bind(&StringCallback::callback, &str_callback, std::placeholders::_1));
 
   rclcpp::executors::SingleThreadedExecutor exec;
