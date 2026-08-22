c0b3b09d1b3338a1b07423e361aca2626417a8d0
Souri Rishik <souri.rishik27@gmail.com>
2026-08-22 11:43:39 +0530

    feat: create publisher internally in RealtimePublisher (#573)
    (cherry picked from commit f2378660935cb392fd31dc02f7093898d8c22266)
    
    # Conflicts:
    #	doc/migration.rst
    #	realtime_tools/test/realtime_publisher_tests.cpp

--- realtime_tools/test/realtime_publisher_tests.cpp
+++ realtime_tools/test/realtime_publisher_tests.cpp
@@ -54,6 +54,7 @@ struct StringCallback
   }
 };
 
+<<<<<<< HEAD
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
@@ -138,46 +139,75 @@ TEST(RealtimePublisher, rt_try_publish_legacy)
 #pragma GCC diagnostic pop
 
 TEST(RealtimePublisher, rt_can_try_publish)
+=======
+void verify_publisher(
+  RealtimePublisher<StringMsg> & rt_pub, const std::shared_ptr<rclcpp::Node> & node,
+  const rclcpp::QoS & qos, const std::string & topic)
+>>>>>>> f237866 (feat: create publisher internally in RealtimePublisher (#573))
 {
-  rclcpp::init(0, nullptr);
-  const size_t ATTEMPTS = 10;
-  const std::chrono::milliseconds DELAY(250);
-
   const char * expected_msg = "Hello World";
-  auto node = std::make_shared<rclcpp::Node>("construct_move_destruct");
-  rclcpp::QoS qos(10);
-  qos.reliable().transient_local();
-  auto pub = node->create_publisher<StringMsg>("~/rt_publish", qos);
-  RealtimePublisher<StringMsg> rt_pub(pub);
   ASSERT_TRUE(rt_pub.can_publish());
 
-  // try publish a latched message
-  bool publish_success = false;
-  for (std::size_t i = 0; i < ATTEMPTS; ++i) {
-    StringMsg msg;
-    msg.string_value = expected_msg;
+  StringMsg msg;
+  msg.string_value = expected_msg;
+  ASSERT_TRUE(rt_pub.try_publish(msg));
 
-    if (rt_pub.can_publish()) {
-      ASSERT_TRUE(rt_pub.try_publish(msg));
-      publish_success = true;
-    }
-    std::this_thread::sleep_for(DELAY);
-  }
-  ASSERT_TRUE(publish_success);
-
-  // make sure subscriber gets it
+  // make sure subscriber gets it (created after publishing to verify transient_local latching)
   StringCallback str_callback;
-
   auto sub = node->create_subscription<StringMsg>(
-    "~/rt_publish", qos,
-    std::bind(&StringCallback::callback, &str_callback, std::placeholders::_1));
+    topic, qos, std::bind(&StringCallback::callback, &str_callback, std::placeholders::_1));
 
   rclcpp::executors::SingleThreadedExecutor exec;
   exec.add_node(node);
-  for (size_t i = 0; i < ATTEMPTS && str_callback.msg_.string_value.empty(); ++i) {
+
+  auto start_time = std::chrono::steady_clock::now();
+  while (str_callback.msg_.string_value.empty() &&
+         (std::chrono::steady_clock::now() - start_time) < std::chrono::seconds(2)) {
     exec.spin_some();
-    std::this_thread::sleep_for(DELAY);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
+
   EXPECT_STREQ(expected_msg, str_callback.msg_.string_value.c_str());
+}
+
+TEST(RealtimePublisher, rt_can_try_publish)
+{
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("construct_move_destruct");
+  rclcpp::QoS qos(10);
+  qos.reliable().transient_local();
+
+  RealtimePublisher<StringMsg> rt_pub(node, "~/rt_publish", qos);
+  verify_publisher(rt_pub, node, qos, "~/rt_publish");
+
+  rclcpp::shutdown();
+}
+
+TEST(RealtimePublisher, rt_can_try_publish_deprecated)
+{
+  rclcpp::init(0, nullptr);
+  auto node = std::make_shared<rclcpp::Node>("construct_move_destruct_deprecated");
+  rclcpp::QoS qos(10);
+  qos.reliable().transient_local();
+  auto pub = node->create_publisher<StringMsg>("~/rt_publish_dep", qos);
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#endif
+#if defined(__clang__) || defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+  RealtimePublisher<StringMsg> rt_pub(pub);
+#if defined(__clang__) || defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+
+  verify_publisher(rt_pub, node, qos, "~/rt_publish_dep");
+
   rclcpp::shutdown();
 }
